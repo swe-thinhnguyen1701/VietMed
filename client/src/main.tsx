b4de8cce110d77c00e7ef167d5304162b9b2123d
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-07-12 16:37:30 -0700

    Integerate color theme

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.tsx'
-import theme from './theme.ts'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ChakraProvider>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </StrictMode>,
